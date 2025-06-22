1ad3d53cb2822915b9e68811ee914230e347014e
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-06-22 12:12:12 -0500

    Clase Tienda  casi completa con estructura base

--- src/Conexion/Conexiondb.java
+++ src/Conexion/Conexiondb.java
@@ -1,5 +1,28 @@
 package Conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexiondb {
 
+  private static final String URL = "jdbc:mysql://localhost:3306/tu_basededatos";
+  private static final String USUARIO = "root";
+  private static final String CONTRASENA = "contrasena";
+
+  public Connection conectar() {
+
+    Connection conexion = null;
+
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+      System.out.println("Conexión exitosa a la base de datos MySQL.");
+
+    } catch (ClassNotFoundException | SQLException e) {
+      System.err.println("Error de conexión: " + e.getMessage());
+    }
+
+    return conexion;
+  }
 }
--- src/Mundo/Tienda.java
+++ src/Mundo/Tienda.java
@@ -1,5 +1,31 @@
 package Mundo;
 
 public class Tienda {
+	private String nombre;
+	private String direccion;
+	  
+	private String contacto;
+
+	  
+	public Tienda(String pnombre, String pdireccion, String pcontacto) {
+		nombre = pnombre;
+	    direccion = pdireccion;
+	    contacto = pcontacto;
+	}
+
+	  
+	public String getNombre() {
+	    return this.nombre;
+	}
+
+	  
+	public String getDireccion() {
+	    return this.direccion;
+	}
+
+	  
+	public String getContacto() {
+	    return this.contacto;
+	}
 
 }
