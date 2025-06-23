238b4479217f02d82bcb7156b07ced3a68153618
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-06-23 11:40:11 -0500

    Creacion de la clase categoria con su DAO, estructura basica de categoria

---
+++ src/Mundo/Categoria.java
@@ -0,0 +1,27 @@
+package Mundo;
+
+public class Categoria {
+  private int id;
+  private String nombre;
+
+  public Categoria(String pnombre) {
+    this.nombre = pnombre;
+  }
+
+  public Categoria(int pid, String pnombre) {
+    this.id = pid;
+    this.nombre = pnombre;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public String getNombre() {
+    return this.nombre;
+  }
+
+  public void setNombre(String pnombre) {
+    this.nombre = pnombre;
+  }
+}
