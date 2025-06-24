da04399d792e6f3f3291ea0e71e3cf69f264c7d0
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2025-06-24 07:07:08 -0500

    Refactorización de código en Interfaz

--- src/Interfaz/PanelBanner.java
+++ src/Interfaz/PanelBanner.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 public class PanelBanner extends JPanel {
     public PanelBanner() {
         setLayout(new FlowLayout(FlowLayout.CENTER));
-        setBackground(new Color(70, 130, 180)); // Color azul profesional
+        setBackground(new Color(70, 130, 180)); 
         
         JLabel titulo = new JLabel("DIRECTORIO DE TIENDAS");
         titulo.setFont(new Font("Arial", Font.BOLD, 20));
